cb95a023fb209f745549257764e546c194d22b18
Samuel Vicente <samuel.eduardo.vicente@gmail.com>
2019-12-03 14:35:32 -0600

    Putting together main page and added provider and store to App.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import {
   BrowserRouter as Router,
+  Switch,
+  Route
 } from "react-router-dom";
 import './App.css';
 import Header from './components/Header';
-
+import Main from './pages/Main'
 function App() {
   return (
     <Router>
       <Header />
+      <Switch>
+        <Route exact path='/'>
+          <Main />
+        </Route>
+      </Switch>
     </Router>
   );
 }
---
+++ src/pages/Main/index.js
@@ -0,0 +1,48 @@
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+import { Col } from 'reactstrap';
+import * as actions from './actions'
+import { bindActionCreators } from 'redux';
+import InfoSearch from '../../components/InfoSearch';
+class Main extends PureComponent {
+  onSearchChange = (e) => {
+    this.props.actions.searchValueChanged(e.currentTarget.value)
+    if (this.props.toolTipOpen) {
+      this.props.actions.openToolTip(false)
+    }
+  }
+
+  searchTerm = async () => {
+    await this.props.actions.isLoading(true)
+    await this.props.actions.apiRequest(this.props.searchValue, this.props.weirdValue)
+    await this.props.actions.isLoading(false)
+  }
+
+  render() {
+    return (
+      <>
+        <div className="App">
+          <Col xs="6">
+            <InfoSearch search={this.searchTerm} onChange={this.onSearchChange} searchValue={this.props.searchValue} toolTipOpen={this.props.toolTipOpen} />
+          </Col>
+        </div>
+      </>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    searchValue: state.HomeReducer.searchValue,
+    weirdValue: state.HomeReducer.weirdValue,
+    toolTipOpen: state.HomeReducer.toolTipOpen
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(actions, dispatch)
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Main)
