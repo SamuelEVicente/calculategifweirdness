1a1309bf202410a8d999dfc610b268d1051de834
Samuel Vicente <samuel.eduardo.vicente@gmail.com>
2019-12-03 15:15:26 -0600

    Adding gif component, going to hold result in SearchResult section

---
+++ src/components/Gif/Gif.test.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { findByTestAtrr } from '../../utils/testUtils';
+import Gif from './index';
+
+const setUp = (props = {
+  onUnlikeGif: false,
+  gif: {
+    title: 'Test',
+    url: 'Test.Url'
+  }
+}) => {
+  const component = shallow(<Gif {...props} />);
+  return component;
+};
+
+describe('Gif Component', () => {
+
+  let component;
+  beforeEach(() => {
+    component = setUp();
+  });
+
+  it('Should render without errors', () => {
+    const wrapper = findByTestAtrr(component, 'Gif');
+    expect(wrapper.length).toBe(1);
+  });
+
+  it('Should render <img> with Gif', () => {
+    const gifImage = findByTestAtrr(component, 'Gif-Image');
+    expect(gifImage.length).toBe(1);
+    expect(gifImage.props().alt).toBe('Test')
+    expect(gifImage.props().src).toBe('Test.Url')
+    const gifTitle = findByTestAtrr(component, 'Gif-Title');
+    expect(gifTitle.props().children).toBe('Test')
+  });
+
+  it('Should not render Unlike button', () => {
+    const gifUnlike = findByTestAtrr(component, 'Gif-Unlike');
+    expect(gifUnlike.length).toBe(0);
+  })
+});
---
+++ src/components/Gif/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+import { AiFillDislike } from 'react-icons/ai'
+import PropTypes from 'prop-types';
+import './styles.css'
+
+const Gif = (props) => {
+  const unLikeable = props.onUnlikeGif ? <Button data-test="Gif-Unlike" id={props.gif.id} onClick={e => props.onUnlikeGif(e)}><AiFillDislike /> </Button> : null;
+  return (
+    <section data-test="Gif" className="gif">
+      <h5 data-test="Gif-Title">{props.gif.title}</h5>
+      <img data-test="Gif-Image" width={props.width} height={props.height} src={props.gif.url} alt={props.gif.title} />
+      {unLikeable}
+    </section>
+  );
+}
+
+Gif.propTypes = {
+  onUnlikeGif: PropTypes.bool,
+  gif: PropTypes.object,
+  width: PropTypes.string,
+  height: PropTypes.string
+}
+
+export default Gif
