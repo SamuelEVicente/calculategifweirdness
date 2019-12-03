7bc3c925d2df8177ec7a551917b862de5b1dcb24
Samuel Vicente <samuel.eduardo.vicente@gmail.com>
2019-12-03 08:59:01 -0600

    Implemented unit test for Header component. Added test configuration for enzyme, and also added helper function that all tests will use.

---
+++ src/__tests__/components/Header.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { findByTestAtrr } from '../../utils/testUtils';
+import Header from '../../components/Header';
+
+const setUp = (props = {}) => {
+  const component = shallow(<Header {...props} />);
+  return component;
+};
+
+describe('Header Component', () => {
+
+  let component;
+  beforeEach(() => {
+    component = setUp();
+  });
+
+  it('Should render without errors', () => {
+    const wrapper = findByTestAtrr(component, 'Header');
+    expect(wrapper.length).toBe(1);
+  });
+
+  it('Should render a logo', () => {
+    const brand = findByTestAtrr(component, 'Header-Brand');
+    expect(brand.length).toBe(1);
+    expect(brand.props().children).toBe('Weirdness Calculator')
+  });
+});
